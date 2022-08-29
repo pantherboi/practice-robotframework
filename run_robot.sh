f34c43d7836f71e999831de27a71fe0fc2a6ebb6
sjittikhun <jittikhun@depth1st.com>
2022-08-29 17:42:02 +0700

    update shell, add webdriver to PATH

--- run_robot.sh
+++ run_robot.sh
@@ -5,5 +5,6 @@ outputdir=$file/../../test-result
 date=$(date "+%Y%m%d")
 timestamp=$(date "+%Y%m%d%H%M%s")
 /usr/local/bin/chromedriver --version
+PATH=$PATH:/usr/local/bin
 export ROBOT_OPTIONS="--outputdir $outputdir/$date/$timestamp --timestampoutputs --variable field:$value"
 robot $1 
